3c9ff53804a21d5cb71d1c44f8b1f0f68be080d2
zhangshengjie <z.cejay@gmail.com>
2023-02-15 14:30:52 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+/*
+ * @Description: SafeSeer
+ * @Version: 1.0
+ * @Autor: z.cejay@gmail.com
+ * @Date: 2023-02-15 14:28:27
+ * @LastEditors: cejay
+ * @LastEditTime: 2023-02-15 14:29:27
+ */
+fn main() {
+    println!("Hello, SafeSeer!");
+}
